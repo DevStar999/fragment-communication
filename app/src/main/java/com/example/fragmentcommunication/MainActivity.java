b79c14e7381377afadfff8aa3eb3f71a5272326a
DevStar999 <wolverinerage99@gmail.com>
2022-08-27 08:50:54 +0530

    Setting up the SecondActivity for further learning of working with Fragments

--- app/src/main/java/com/example/fragmentcommunication/MainActivity.java
+++ app/src/main/java/com/example/fragmentcommunication/MainActivity.java
@@ -2,8 +2,15 @@ package com.example.fragmentcommunication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
+/**
+ * The classes MainActivity, FragmentA & FragmentB are associated with each other for the purpose of
+ * learning how to communicate between 2 fragments via their parent activity. In here, we also learned
+ * how to communicate between the fragment and it's parent activity.
+ */
 public class MainActivity extends AppCompatActivity implements FragmentA.FragmentAListener, FragmentB.FragmentBListener {
     private FragmentA fragmentA;
     private FragmentB fragmentB;
@@ -36,5 +43,11 @@ public class MainActivity extends AppCompatActivity implements FragmentA.Fragmen
     public void onInputBSent(CharSequence input) {
         // Data from FragmentA is being sent to FragmentB
         fragmentA.updateEditText(input);
+    }
+
+    public void gotoSecondActivity(View view) {
+        Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+        startActivity(intent);
+        finish();
     }
 }

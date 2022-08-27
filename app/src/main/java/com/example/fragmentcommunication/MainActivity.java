6a0edbe0d9c3f3398d74107610a13bf6e87d7823
DevStar999 <wolverinerage99@gmail.com>
2022-08-27 08:41:55 +0530

    Wiring up the 2 fragments together

--- app/src/main/java/com/example/fragmentcommunication/MainActivity.java
+++ app/src/main/java/com/example/fragmentcommunication/MainActivity.java
@@ -4,11 +4,37 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements FragmentA.FragmentAListener, FragmentB.FragmentBListener {
+    private FragmentA fragmentA;
+    private FragmentB fragmentB;
+
+    private void initialise() {
+        fragmentA = new FragmentA();
+        fragmentB = new FragmentB();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initialise();
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.container_a, fragmentA)
+                .replace(R.id.container_b, fragmentB)
+                .commit();
+    }
+
+    @Override
+    public void onInputASent(CharSequence input) {
+        // Data from FragmentA is being sent to FragmentB
+        fragmentB.updateEditText(input);
+    }
+
+    @Override
+    public void onInputBSent(CharSequence input) {
+        // Data from FragmentA is being sent to FragmentB
+        fragmentA.updateEditText(input);
     }
 }
